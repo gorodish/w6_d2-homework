126808caf0750367167b644f3977a34f577cd3c3
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-02-14 15:33:12 +0000

    initial commit

---
+++ Human.java
@@ -0,0 +1,10 @@
+public class Human implements Edible, Swimmable {
+
+  public String speak() {
+    return "Help I'm being eaten by a bear!";
+  }
+
+  // public String swim() {
+  //   return "Swimming";
+  // }
+}
